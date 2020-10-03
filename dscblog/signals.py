7115ddedbb921081dd9265fdcd80c53287811c82
Asrient <asen182000@hotmail.com>
2020-10-03 22:10:38 +0530

    LogIn with Google now saves name and avatar too

---
+++ dscblog/signals.py
@@ -0,0 +1,14 @@
+from allauth.account.signals import user_signed_up
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+
+@receiver(user_signed_up)
+def populate_profile(sociallogin, user, **kwargs):
+    if sociallogin.account.provider == 'google':
+        user_data = user.socialaccount_set.filter(provider='google')[0].extra_data
+        if 'name' in user_data:
+            user.name=user_data['name']
+        if 'picture' in user_data:
+            user.avatar_url=user_data['picture']
+        user.save()
