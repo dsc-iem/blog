ee2dd86fe79be7e6f89166f7da238e810fba8266
Asrient <asen182000@hotmail.com>
2020-12-16 23:30:42 +0530

    Fixed issue #46

--- dscblog/signals.py
+++ dscblog/signals.py
@@ -4,11 +4,13 @@ from django.contrib.auth.models import User
 
 
 @receiver(user_signed_up)
-def populate_profile(sociallogin, user, **kwargs):
-    if sociallogin.account.provider == 'google':
-        user_data = user.socialaccount_set.filter(provider='google')[0].extra_data
-        if 'name' in user_data:
-            user.name=user_data['name']
-        if 'picture' in user_data:
-            user.avatar_url=user_data['picture']
-        user.save()
+def populate_profile(sociallogin=None, user=None, **kwargs):
+    if user != None and sociallogin != None:
+        if sociallogin.account.provider == 'google':
+            user_data = user.socialaccount_set.filter(
+                provider='google')[0].extra_data
+            if 'name' in user_data:
+                user.name = user_data['name']
+            if 'picture' in user_data:
+                user.avatar_url = user_data['picture']
+            user.save()
